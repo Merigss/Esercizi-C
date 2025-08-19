f2b22944b68c05263a9e66db917b7856325bce57
Merigss <stefanomerigo99@gmail.com>
2025-08-19 09:30:43 +0200

    fine

--- Esercizio2.c
+++ Esercizio2.c
@@ -3,9 +3,26 @@
 #include <time.h>
 
 int main (void){
-    int num;
+    srand(time(NULL))
+    int num, volte=0, x;
 
     do{
+        printf("Inserisci un numero\n");
+        scanf("%d",&num);
 
-    }while()
+    }while(num<0 || num >=20)
+
+
+    for(int i=0; i<100; i++){
+        x=(rand()%(20-1+1))+1;
+        printf("Il numero random è: %d\n",x);
+        if(x==num){
+            volte++;
+        }
+
+    }
+
+    printf("Il numero di volte che %d compare nella sequenza di numeri casuali è %d",num,volte);
+
+    return 0;
 }
