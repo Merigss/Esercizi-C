d0275f7637ed557fed197e6669206a2e7eb9c607
Merigss <stefanomerigo99@gmail.com>
2025-08-19 11:42:04 +0200

    pipopipo

---
+++ Esercizio 2 070121 v2/Esercizio 2 070121 v2.c
@@ -0,0 +1,5 @@
+/*Scrivere un programma C che acquisisca da tastiera 20 numeri naturali in base 10
+e successivamente visualizzi la quantità totale di cifre che costituiscono le codifiche in base 2 dei 
+numeri acquisiti. Per svolgere questo compito si definisca ed utilizzi una funzione che produca il
+numero di cifre che costituiscono la codifica binaria di un dato intero in base 10
+*/
