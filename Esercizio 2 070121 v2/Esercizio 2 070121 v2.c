1faf188fcd6aa50eb5cb55a6d85b433ddbb2f90f
Merigss <stefanomerigo99@gmail.com>
2025-08-19 11:50:48 +0200

    quasi finito

--- Esercizio 2 070121 v2/Esercizio 2 070121 v2.c
+++ Esercizio 2 070121 v2/Esercizio 2 070121 v2.c
@@ -3,3 +3,22 @@ e successivamente visualizzi la quantità totale di cifre che costituiscono le c
 numeri acquisiti. Per svolgere questo compito si definisca ed utilizzi una funzione che produca il
 numero di cifre che costituiscono la codifica binaria di un dato intero in base 10
 */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int codifica (int num){
+    int codificato;
+    while()
+}
+
+int main (void){
+
+    int num;
+
+    for(int i=0;i<20;i++){
+        printf("Inserisci un numero naturale: ");
+        scanf("%d",&num);
+        printf("\nIl numero %d scritto in cifre binarie corrisponde a %d\n",num,codifica(num));
+    }
+}
