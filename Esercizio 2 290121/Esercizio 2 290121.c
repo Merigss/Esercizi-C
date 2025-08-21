9e3abbe7e649533b402fda53260f1bc0150f805d
Merigss <stefanomerigo99@gmail.com>
2025-08-21 16:46:43 +0200

    .

---
+++ Esercizio 2 290121/Esercizio 2 290121.c
@@ -0,0 +1,11 @@
+/**
+ * Scrivere un programma C che riempia una sequenza con 50 interi pseudo-casuali
+compresi tra 2 e 100 (estremi inclusi) e successivamente per ciascuno intero x della sequenza
+visualizzi la coppia di interi a e b tale per cui a ⋅ b è uguale a x e la somma a + b è la più più
+piccola tra le somme delle coppie di interi il cui prodotto è pari ad x. Per svolgere questo compito
+si definisca ed utilizzi una funzione che inizializzi gli interi puntati da due dati puntatori con la
+coppia di interi tale per cui il prodotto degli interi della coppia è uguale ad un dato intero e la
+somma degli interi della coppia è la più piccola tra le somme delle coppie di interi il cui prodotto
+è pari al dato intero.
+ */
+
